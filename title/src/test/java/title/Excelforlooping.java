67ec6a0cd6eefe793ca55b224e92c78b1c17c31a
niraj <niraj@DESKTOP-UI77LPV>
2024-05-05 09:44:18 +0530

    update

--- title/src/test/java/title/Excelforlooping.java
+++ title/src/test/java/title/Excelforlooping.java
@@ -8,13 +8,17 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbookFactory;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.devtools.DevTools;
+import org.openqa.selenium.edge.EdgeDriver;
 
 
 
 
 
 public class Excelforlooping {
-
+public static WebDriver driver;
 	public static void main(String[] args) throws Exception {
 		File file= new File("./File input/Practice1.xlsx");
 		FileInputStream fis= new FileInputStream(file);
@@ -26,11 +30,23 @@ public class Excelforlooping {
 			Row r= s.getRow(i);
 		int cellcount=r.getPhysicalNumberOfCells();
 		for(int j=0; j<=cellcount; j++) {
-	     Cell c=r.getCell(j);
-	     System.out.print("||  "+" "+c);
-		}System.out.println("  ");
+	     Cell c=r.getCell(j);{
+	   //String data=  c.getRow().getStringCellValue()
+		//if(data=="k098") {
+			
 		}
+	     
+	     System.out.print("||  "+" "+c);
+		}
+		
+		System.out.println("  ");
+		}
+		
+		
+
 	}
 	
-
-}
+		
+			
+		}
+//}
